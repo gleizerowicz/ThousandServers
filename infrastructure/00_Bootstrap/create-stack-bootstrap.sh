07e037b357b5b2abceb0e50e8de42c9fa0473670
John Miller <John.Miller@neudesic.com>
2017-06-09 10:46:57 -0700

    Document script assumptions

--- infrastructure/00_Bootstrap/create-stack-bootstrap.sh
+++ infrastructure/00_Bootstrap/create-stack-bootstrap.sh
@@ -1,4 +1,5 @@
-aws cloudformation create-stack 
-	--stack-name myBootstrapStack 
-	--template-body file:///mnt/c/git/ThousandAndOneServers/infrastructure/00_Bootstrap/bootstrap.yml 
+# Assumes 00_Bootstrap is the current folder
+aws cloudformation create-stack          \
+	--stack-name myBootstrapStack        \ 
+	--template-body file://bootstrap.yml \ 
 	--capabilities CAPABILITY_NAMED_IAM
--- infrastructure/00_Bootstrap/reset-dynamodb-item.ps1
+++ infrastructure/00_Bootstrap/reset-dynamodb-item.ps1
@@ -1,3 +1,4 @@
+# Assumes 00_Bootstrap is the current folder
 aws dynamodb put-item `
     --table-name "app" `
     --item file://dynamodb.json
